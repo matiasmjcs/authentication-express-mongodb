9ef7dae19dc5fe28df55fc3c4ea1093bceeb4022
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-11 00:37:18 -0300

    Added Update method

--- src/controllers/hotel.controlles.ts
+++ src/controllers/hotel.controlles.ts
@@ -1,32 +1,30 @@
 import { Request, Response } from "express";
+import { createHotel, FindHotelAll, FindHotelById, updateHotel } from "../dataBaseManager/hotel";
+import { IHotelControllers } from "../interfaces/hotel/hotelControllers.interface";
 
-import { initializeDatabaseManager } from "../dataBaseManager/user";
-import { createHotel, FindHotelAll, FindHotelById } from "../dataBaseManager/hotel";
-
-export class HotelControllers {
-    constructor() {
-        initializeDatabaseManager()
-      }
-      async findAll(_req: Request, res:Response) {
-        const response = await FindHotelAll()
-        return res.json({
-          response
-        })
-      }
-      async findById(req: Request, res:Response) {
-        const params = req.params
-        const {id} = params
-        const response = await FindHotelById(id)
-        return res.json({
-          response
-        })
-      }
-      async create(req: Request, res:Response) {
-        const reqBody = req.body
-        const response = await createHotel(reqBody)
-        return res.json({response})
-      }
-      async update(req: Request, res:Response) {
-        return res.json({msg:"update hotel"})
-      }
+export class HotelControllers implements IHotelControllers {
+  async findAll(_req: Request, res: Response) {
+    const response = await FindHotelAll()
+    return res.json({
+      response
+    })
+  }
+  async findById(req: Request, res: Response) {
+    const { id } = req.params
+    const response = await FindHotelById(id)
+    return res.json({
+      response
+    })
+  }
+  async create(req: Request, res: Response) {
+    const reqBody = req.body
+    const response = await createHotel(reqBody)
+    return res.json({ response })
+  }
+  async update(req: Request, res: Response) {
+    const { id } = req.params
+    const body = req.body
+    const response = await updateHotel(id, body)
+    return res.json({ response })
+  }
 }
--- src/dataBaseManager/hotel.ts
+++ src/dataBaseManager/hotel.ts
@@ -1,36 +1,36 @@
-import { IHotelMongodbFetch } from "../interfaces/hotel.interface"
+import { IHotelMongodb, IHotelMongodbFetch } from "../interfaces/hotel/hotel.interface"
 import Hotel from "../models/hotel.models"
 
-export const FindHotelAll = async() => {
+export const FindHotelAll = async () => {
     try {
         const hotels = await Hotel.find()
-        if(!hotels) {throw new Error("no se a encontrado la lista de hoteles")}
+        if (!hotels) { throw new Error("no se a encontrado la lista de hoteles") }
         return hotels
     } catch (error) {
         return {
             success: false,
             error: "DatabaseManager: FindHotelAll Internal server error" + error,
-          };
+        };
     }
 }
 
-export const FindHotelById = async(id: string) => {
+export const FindHotelById = async (id: string) => {
     try {
-        const hotel = await Hotel.findOne({_id:id})
-        if(!hotel) {throw new Error("no se a encontrado el hotel")}
+        const hotel = await Hotel.findOne({ _id: id })
+        if (!hotel) { throw new Error("no se a encontrado el hotel") }
         return hotel
     } catch (error) {
         return {
             success: false,
             error: "DatabaseManager: FindHotelById Internal server error" + error,
-          };
+        };
     }
 }
 
-export const createHotel = async (hotel:IHotelMongodbFetch) => {
+export const createHotel = async (hotel: IHotelMongodb) => {
     try {
-        const hotelVerify = await Hotel.findOne({_id:hotel._id})
-        if(hotelVerify) {throw new Error("este hotel ya existe")}
+        const hotelVerify = await Hotel.findOne({ name: hotel.name })
+        if (hotelVerify) { throw new Error("este hotel ya existe") }
         const newHotel = new Hotel({
             name: hotel.name,
             description: hotel.description,
@@ -38,12 +38,39 @@ export const createHotel = async (hotel:IHotelMongodbFetch) => {
             contact: hotel.contact
         })
         const saveHotel = await newHotel.save()
-        if(!saveHotel){throw new Error("a ocurrido un error al generar el hotel")}
+        if (!saveHotel) { throw new Error("a ocurrido un error al generar el hotel") }
         return { success: true, hotel: saveHotel };
     } catch (error) {
         return {
             success: false,
             error: "DatabaseManager: createHotel Internal server error" + error,
-          };
+        };
+    }
+}
+
+export const updateHotel = async (id: string, updatedData: IHotelMongodb) => {
+    try {
+        const hotel = await Hotel.findOne({ _id: id });
+        if (!hotel) {
+            throw new Error("No se ha encontrado el hotel a actualizar");
+        }
+
+        if(updatedData.name )hotel.name = updatedData.name;
+        if (updatedData.description)hotel.description = updatedData.description;
+        if(updatedData.location)hotel.location = updatedData.location;
+        if(updatedData.contact)hotel.contact = updatedData.contact;
+
+        const updatedHotel = await hotel.save();
+
+        if (!updatedHotel) {
+            throw new Error("Ocurrió un error al actualizar el hotel");
+        }
+
+        return { success: true, hotel: updatedHotel };
+    } catch (error) {
+        return {
+            success: false,
+            error: "DatabaseManager: updateHotel Internal server error" + error,
+        };
     }
 }
