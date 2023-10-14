506d33780d03f4b399c30d890f249518cb8d42df
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-13 21:57:11 -0300

    #	modified:   src/utils/superTest.utils.ts

--- src/utils/superTest.utils.ts
+++ src/utils/superTest.utils.ts
@@ -5,6 +5,12 @@ interface RouteFunction {
 	(app: Application): void;
   }
 
+/**
+ * Creates a test server using the given route function and returns a supertest instance.
+ *
+ * @param {RouteFunction} route - The route function to be used by the server.
+ * @return {supertest} - A supertest instance representing the test server.
+ */
 export function testServer(route:RouteFunction) {
 	const app:Application = express();
 	app.use(express.json());
