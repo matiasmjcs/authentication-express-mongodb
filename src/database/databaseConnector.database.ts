1364d2fcc49f7a1a2d25c19fdede4c361c571d74
matiasmjcs <matiasmejiascisternas@gmail.com>
2023-10-13 22:36:03 -0300

    #	modified:   src/database/databaseConnector.database.ts

--- src/database/databaseConnector.database.ts
+++ src/database/databaseConnector.database.ts
@@ -5,6 +5,11 @@ config();
 
 let mongooseInstance: Mongoose | null = null;
 
+/**
+ * Connects to the MongoDB database.
+ *
+ * @return {Promise<void>} - A promise that resolves when the connection is successful, or rejects with an error.
+ */
 async function connect() {
   try {
     if (!mongooseInstance) {
@@ -15,6 +20,11 @@ async function connect() {
   }
 }
 
+/**
+ * Disconnects from the database if a connection instance exists.
+ *
+ * @return {Promise<void>} - A Promise that resolves once the disconnection is complete.
+ */
 async function disconnect() {
   try {
     if (mongooseInstance) {
